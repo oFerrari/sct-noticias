4d65cd159b42ce01aa67106e765825e177badd30
Andrei Ferrari <113152251+oFerrari@users.noreply.github.com>
2022-10-07 20:55:44 -0400

    Estilizando Topo

--- src/Topo.jsx
+++ src/Topo.jsx
@@ -3,7 +3,7 @@ import logo from './assets/logo-sct.png'
 export function Topo(){
     return(
         <header className="topo">
-            <img src="{logo}" alt="" />
+            <img src={logo} alt="" />
             <h1>SeLigaNaSCT</h1>
         </header>
     )
