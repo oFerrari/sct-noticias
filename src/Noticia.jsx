0f9106be87e87bb39c5931ca2dfa916762c2ad06
Andrei Ferrari <113152251+oFerrari@users.noreply.github.com>
2022-10-07 21:04:55 -0400

    Criando noticias

---
+++ src/Noticia.jsx
@@ -0,0 +1,8 @@
+export function Noticia(){
+    return(
+        <article className="noticia">
+            <h1>Titulo da Noticia</h1>
+            <img src="" alt="" />
+        </article>
+    )
+}
