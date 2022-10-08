54031253add77256a35fc4e61b1b140cb01ad6c3
Andrei Ferrari <113152251+oFerrari@users.noreply.github.com>
2022-10-07 21:11:05 -0400

    Subindo CSS noticia

--- src/Noticia.jsx
+++ src/Noticia.jsx
@@ -1,8 +1,10 @@
+
+
 export function Noticia(){
     return(
         <article className="noticia">
             <h1>Titulo da Noticia</h1>
-            <img src="" alt="" />
+            <img src="https://api.lorem.space/image?w=500&h=500" alt="" />
         </article>
     )
 }
